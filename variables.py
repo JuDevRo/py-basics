01556baa02abe378b988ab4debe004b123bc10d9
judev <76juandevi@gmail.com>
2026-04-28 14:53:29 +0200

    variables

---
+++ variables.py
@@ -0,0 +1,27 @@
+x = "Esta es una variable"
+
+print(x + ' ' + 'hola')
+
+x = "Otra cosa"
+
+print(x)
+
+X = "Comoooo"
+
+print(X, x)
+
+mivariable = "Variable"
+mi_variable = "Otra"
+_my_var=  "a"
+miVar = "b"
+MIVARIABLE = "c"
+mivariable2 = "a"
+
+# Esto da error
+# 2var = "b"
+# mi-varr = "a"
+# mi var = "afdsa"
+
+camelCase = "Comienza con minuscula y cada palabra siguiente con mayuscula"
+PascalCase = "Cada palabra comienza con mayuscula"
+snake_case = "Todas las palabras con minuscula y separadas por guion bajo"
