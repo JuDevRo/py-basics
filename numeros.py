062e562292115ec70703618b0984510a07d17766
judev <76juandevi@gmail.com>
2026-04-28 15:44:53 +0200

    numbers

---
+++ numeros.py
@@ -0,0 +1,25 @@
+x = 1
+y = 2.5
+z = 1j
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+xf = float(x)
+
+print(xf)
+print(type(xf))
+
+ye = int(y)
+
+print(ye)
+print(type(ye))
+
+entero = 5
+
+eCom = complex(entero)
+
+import random
+
+print(random.randrange(1, 10))
