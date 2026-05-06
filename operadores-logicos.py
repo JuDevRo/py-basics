d4cf66b7ac393dfeecd8b15f40030f58704f8a70
judev <76juandevi@gmail.com>
2026-05-06 17:53:24 +0200

    operadores logicos

---
+++ operadores-logicos.py
@@ -0,0 +1,24 @@
+# Comparación
+
+x = 5
+y = 3
+z = 5
+
+print(x == y)
+print(x != y)
+print(x > y)
+print(x > z)
+print(x < y)
+print(x >= y)
+print(x >= z)
+print(x <= y)
+
+# Operadores lógicos
+
+print(x > y and y > z)
+print(x > y or y > z)
+
+v  = True
+f = False
+print(not(v))
+print(not(f))
