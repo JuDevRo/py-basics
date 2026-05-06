9a7dde472290915c00b80df35f4dacf0e1983d9e
judev <76juandevi@gmail.com>
2026-05-06 14:21:41 +0200

    operadores

---
+++ operadores.py
@@ -0,0 +1,36 @@
+# Operadores artmeticos
+
+x = 5
+y = 10
+
+# Suma
+print("Suma", x + y)
+
+# Resta
+print("Resta", x - y)
+
+#  Multiplicacion
+print("Multiplicacion", x * y)
+
+# Division
+print("Division", x / y)
+
+# Modulo (resto)
+print("Modulo", x % y)
+
+# Potencia
+print("Potencia", x ** y)
+
+# Division entera
+print("Division entera", x // y)
+
+par = 8
+print(par % 2 == 0)
+
+# Precedencia de operadores
+# Parentesis
+# Exponentes
+# Mutliplicacion y division, divisiones enteras y restos
+# Suma y resta
+# Comparaciones de identidad y pertenencia
+# Lógicos
